1bba1eb33387dbeebaf00215651d214ea4a42ae1
Hydra <umeraqeel11@gmail.com>
2020-04-08 21:50:58 +0500

    Login automation

---
+++ src/test/java/login/LoginTests.java
@@ -0,0 +1,21 @@
+package login;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.SecureAreaPage;
+
+import static org.testng.Assert.*;
+
+public class LoginTests extends BaseTests {
+
+    @Test
+    public void testSucccessfulLogin(){
+        LoginPage loginPage = homepage.clickFormAuthentication();
+        loginPage.setUsername("tomsmith");
+        loginPage.setPassword("SuperSecretPassword!");
+        SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
+        assertTrue(secureAreaPage.getAleartText().contains("You logged into a secure area!"), "Alert text is incorrect");
+
+    }
+}
